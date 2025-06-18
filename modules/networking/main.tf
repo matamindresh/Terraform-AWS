ed788d6f6a1c3d1d6240a3a7aedc82fbb8fbea3a
matamindresh <matamindresh@gmail.com>
2025-06-18 18:52:25 +0530

    updated network file

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -58,7 +58,7 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
